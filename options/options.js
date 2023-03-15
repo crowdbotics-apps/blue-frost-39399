e3abef024916e660ddd8600529b30a6013452d34
Crowdbotics <team@crowdbotics.com>
2023-03-15 12:56:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-frost-39399",
+  url: "https://blue-frost-39399.botics.co",
+  api: "https://blue-frost-39399.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
